2c64c7d407f2bfbc3fc86b7309047691118c6351
Melissa Jeremie <melissajeremie@gmail.com>
2021-02-16 13:00:10 -0500

    mod 2 w1hw1 css

--- Homework/Mod-2/week-1/dashboard/app.js
+++ Homework/Mod-2/week-1/dashboard/app.js
@@ -4,7 +4,11 @@ class App extends React.Component{
     render() {
         return (
             <div id="container">
-
+                <Sidebar />
+                <Reviews />
+                <AvgRating />
+                <SentimentAnalysis />
+                <SiteVisits />
             </div>
         )
     }
@@ -31,8 +35,8 @@ class Reviews extends React.Component{
     render() {
         return (
             <div id="reviews">
-                <h5>Reviews</h5>
-                <p>lorem ipsum</p>
+                <p>Reviews</p>
+                <h5>1,281</h5>
             </div>
         )
     }
@@ -43,6 +47,7 @@ class AvgRating extends React.Component{
         return (
             <div id="avg-rating">
                 <p>Average Rating</p>
+                <h6>4.6</h6>
             </div>
         )
     }
@@ -53,6 +58,9 @@ class SentimentAnalysis extends React.Component{
         return (
             <div id="analysis">
                 <p>Sentiment Analysis</p>
+                <p>960</p>
+                <p>122</p>
+                <p>321</p>
             </div>
         )
     }
@@ -61,8 +69,9 @@ class SentimentAnalysis extends React.Component{
 class SiteVisits extends React.Component{
     render() {
         return (
-            <div id="container">
-
+            <div id="visitors">
+                <p>Website visitors</p>
+                <h5>821</h5>
             </div>
         )
     }
