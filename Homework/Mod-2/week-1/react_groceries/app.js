d0a92922bb571a4e0ca859d76bbf738af7e243dc
Melissa Jeremie <melissajeremie@gmail.com>
2021-02-16 14:40:53 -0500

    mod 2 w1hw2 initial files

---
+++ Homework/Mod-2/week-1/react_groceries/app.js
@@ -0,0 +1,15 @@
+console.log('hello');
+
+const groceries = [ ]
+
+class App extends React.Component{
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("main"));
