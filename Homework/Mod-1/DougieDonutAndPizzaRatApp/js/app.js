fe1c3c1dec69e20dc0411d9c7d92d3bde0eaa809
Melissa Jeremie <melissajeremie@gmail.com>
2021-02-26 22:53:29 -0500

    311 and react groceries update

--- Homework/Mod-1/DougieDonutAndPizzaRatApp/js/app.js
+++ Homework/Mod-1/DougieDonutAndPizzaRatApp/js/app.js
@@ -1,10 +1,34 @@
 console.log("app.js here");
 
-const APIKEY = "1r7ndj6oyulqhosresu3eorpu";
-const TOKEN = "3jIPXBOlxaVpuGrsRKxm6Y2D0";
-const NYPD = "New York City Police Department";
-const URL = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?"; 
+// const APIKEY = "1r7ndj6oyulqhosresu3eorpu";
+// const TOKEN = "3jIPXBOlxaVpuGrsRKxm6Y2D0";
+const URL = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?agency=NYPD&borough="; 
 
-$(function() {
-    
+$(() => {
+    let borough = null;
+    $("button").click(function(event) {
+        event.preventDefault();
+        borough = this.id;
+        console.log(borough);
+
+        const requestUrl = URL + borough;
+        console.log(requestUrl);
+
+        let resultNum = document.getElementsByClassName('user-input').value;
+        console.log(resultNum);
+
+        $.ajax({
+            url: requestUrl,
+            type: "GET",
+            data: {
+                "$limit": resultNum || 10
+            }
+        })
+        .then(function(data) {
+            for (let i = 0; i < data.length; i++) {
+                $(".complaints").append(data[i].complaint_type + " , " + data[i].agency + "<br>");
+            };
+            console.log(data);
+        })
+    })
 })
